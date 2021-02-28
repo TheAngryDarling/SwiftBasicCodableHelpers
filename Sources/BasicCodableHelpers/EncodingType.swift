32c9e39907498a4400da08a939782f1263318531
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2021-02-28 14:41:24 -0500

    Added encodeToSingleOrArray to EncoderType and StandardEncoderType where they extend SupportedDictionaryRootEncoderType and SupportedArrayRootEncoderType

--- Sources/BasicCodableHelpers/EncodingType.swift
+++ Sources/BasicCodableHelpers/EncodingType.swift
@@ -41,9 +41,9 @@ public protocol StandardEncoderType: class {
     func encode<T>(_ value: T) throws -> Data where T : Encodable
 }
 
-// Indicator if the root supports Dictionaries
+/// Indicator if the root supports Dictionaries
 public protocol SupportedDictionaryRootEncoderType { }
-// Indicator if the root supports arrays
+/// Indicator if the root supports arrays
 public protocol SupportedArrayRootEncoderType { }
 
 extension JSONEncoder: DataEncoderType, StandardEncoderType, SupportedDictionaryRootEncoderType, SupportedArrayRootEncoderType { }
@@ -52,4 +52,67 @@ extension JSONEncoder: DataEncoderType, StandardEncoderType, SupportedDictionary
 #if swift(>=5.1) || _runtime(_ObjC)
 extension PropertyListEncoder: DataEncoderType, StandardEncoderType, SupportedDictionaryRootEncoderType, SupportedArrayRootEncoderType { }
 #endif
+
+fileprivate struct CollectionEncoder<C>: Encodable where C: Collection, C.Element: Encodable {
+    let collection: C
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+        for obj in collection {
+            try container.encode(obj)
+        }
+    }
+}
+
+public extension EncoderType where Self: SupportedDictionaryRootEncoderType, Self: SupportedArrayRootEncoderType {
+    /// Provides an easy method to encode an array of encodable objects in a dynamic way
+    ///
+    /// The following rules apply when encoding:
+    /// 1. If collection count is 0, An empty array gets encoded
+    /// 2. If collection count is 1, encodes the object as a single value and not an array
+    /// 3. Encodes as a regular array
+    ///
+    /// - Parameters:
+    ///   - collection: The collection to encode
+    /// - returns: Returns an indicator if the object was encoded or not
+    func encodeToSingleOrArray<C>(_ collection: C) throws -> EncodedData where C: Collection, C.Element: Encodable {
+        
+        if collection.count == 1 {
+            return try self.encode(collection[collection.startIndex])
+        } else {
+            let col = CollectionEncoder(collection: collection)
+            return try self.encode(col)
+        }
+    }
+}
+
+
+
+public extension StandardEncoderType where Self: SupportedDictionaryRootEncoderType, Self: SupportedArrayRootEncoderType {
+    /// Provides an easy method to encode an array of encodable objects in a dynamic way
+    ///
+    /// The following rules apply when encoding:
+    /// 1. If collection count is 0, An empty array gets encoded
+    /// 2. If collection count is 1, encodes the object as a single value and not an array
+    /// 3. Encodes as a regular array
+    ///
+    /// - Parameters:
+    ///   - collection: The collection to encode
+    /// - returns: Returns an indicator if the object was encoded or not
+    func stdEncodeToSingleOrArray<C>(_ collection: C) throws -> Data where C: Collection, C.Element: Encodable {
+
+        if collection.count == 1 {
+            return try self.encode(collection[collection.startIndex])
+        } else {
+            /*let col = CollectionEncoder(collection: collection)
+            return try self.encode(col)*/
+            let encoder = EncoderHelper() { encoder in
+                try encoder.encodeToSingleOrArray(collection)
+            }
+            return  try self.encode(encoder)
+        }
+    }
+}
+
+
 
